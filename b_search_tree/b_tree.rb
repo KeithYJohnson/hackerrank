f455c80f633bd78edd979e240ed22c0f351bf719
Keith Johnson <keithyjohnson@gmail.com>
2017-02-19 14:06:17 -0500

    Adding simple test case for BFS right in b_search_tree/b_tree.  TODO move this into an rspec test.

--- b_search_tree/b_tree.rb
+++ b_search_tree/b_tree.rb
@@ -11,11 +11,36 @@ class BTree
   end
 end
 
-root = Node.new(0)
+# root = Node.new(0)
+# builder = TreeBuilder.new
+# builder.build_tree(root)
+# tree    = BTree.new(root)
+# target = tree.root.left.value
 
-builder = TreeBuilder.new
-builder.build_tree(root)
-tree    = BTree.new(root)
-target = tree.root.left.value
+
+# A test case becasue TreeBuilder is random
+root = Node.new(5)
+
+#1st level
+first_left  = Node.new(3)
+first_right = Node.new(10)
+root.left   = first_left
+root.right  = first_right
+
+# SECOND LEVEL
+# Left Nodes
+left_left  = Node.new(1)
+left_right = Node.new(4)
+first_left.left = left_left
+first_left.right = left_right
+
+#Right nodes
+right_left = Node.new(7)
+right_right = Node.new(12)
+first_right.left = right_left
+first_right.right = right_right
+
+tree = BTree.new(root)
+target = 10
 searcher = BreadthFirstSearch.new(tree, target)
 searcher.perform
