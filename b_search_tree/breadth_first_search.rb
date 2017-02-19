7b525b7fcef5da5513fbef7cb077140f97b10bba
Keith Johnson <keithyjohnson@gmail.com>
2017-02-19 14:05:07 -0500

    BFS is now tracking levels and doing... BFS.

--- b_search_tree/breadth_first_search.rb
+++ b_search_tree/breadth_first_search.rb
@@ -1,31 +1,39 @@
 require 'pry-byebug'
 
 class BreadthFirstSearch
-  attr_accessor :tree, :target, :not_found
+  attr_accessor :tree, :target
+  @@current_level = 0
+  @@not_found     = true
 
   def initialize(tree, target)
     self.tree = tree
     self.target = target
-    self.not_found = true
   end
 
   def perform
     root = tree.root
     return "found target: #{target} at root" if root.value == target
 
-    search_node(root.left, target)
-    search_node(root.right, target)
+    children = [root.left, root.right]
+    search_level(children)
+  end
+
+  def search_level(nodes)
+    @@current_level += 1
+    puts "searching level: #{@@current_level}"
+
+    next_level = []
+    nodes.each do |node|
+      next_level = node.next_level
+      search_node(node, target)
+    end
+    search_level(next_level.flatten) if @@not_found && !next_level.compact.empty?
   end
 
   def search_node(node, target)
-    while not_found
-      if node && node.value == target
-        puts "Found target: #{target} at node: #{node}, node.value #{node.value}"
-        not_found = false
-      end
-
-      search_node(node.left, target)  if node.left
-      search_node(node.right, target) if node.right
+    if node && node.value == target
+      puts "Found target: #{target} at node: #{node}, node.value #{node.value}"
+      @@not_found = false
     end
   end
 end
