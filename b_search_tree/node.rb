a641fedebe0799f0e0d4f23217ba3bf7558aa727
Keith Johnson <keithyjohnson@gmail.com>
2017-02-19 13:38:41 -0500

    Auto-incrementing IDs for class Node to make it easier to track which level BFS is currently on.

--- b_search_tree/node.rb
+++ b_search_tree/node.rb
@@ -1,9 +1,12 @@
 class Node
-  attr_accessor :left, :right, :value
+  attr_accessor :left, :right, :value, :id
+  @@current_id = 1
 
   def initialize(value, left=nil, right=nil)
+    self.id    = @@current_id
     self.value = value
-    self.left = left
+    self.left  = left
     self.right = right
+    @@current_id += 1
   end
 end
