f9b28f1ba85199d821cc10c4edd744d172620fc7
Keith Johnson <keithyjohnson@gmail.com>
2017-02-22 11:38:19 -0500

    oops a bug

--- roman/converters.rb
+++ roman/converters.rb
@@ -1,9 +1,12 @@
-
 def roman_to_int(roman_num, value=0)
   map = {
     "I": 1,
     "V": 5,
-    "X": 10
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1_000
   }
 
   roman_num.each_char.with_index do |char, index|
@@ -12,23 +15,22 @@ def roman_to_int(roman_num, value=0)
     unless index + 2 > roman_num.length
       next_char_as_int   = map[roman_num[index + 1].to_sym]
     else
+      # At the end of the string
       value += char_as_int
-      p value
-      return
+      return value
     end
 
-    if index == 0
-      value += char_as_int
-      next
-    end
 
     if next_char_as_int > char_as_int
       value -= char_as_int
     else
       value += char_as_int
     end
-
   end
 end
 
-roman_to_int('XIV')
+p roman_to_int('XIV')
+p roman_to_int('MD')
+p roman_to_int('M')
+p roman_to_int('IV')
+p roman_to_int('III')
