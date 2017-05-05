86d71dd5cae30ed09bd552919a62e39e2a504183
Keith Johnson <keithyjohnson@gmail.com>
2017-05-05 09:24:03 -0400

    iterative solution for phone number letter permuattions

--- dynamic/phone_letters/run.rb
+++ dynamic/phone_letters/run.rb
@@ -28,3 +28,22 @@ def solution(number, permuts=[[]], orig_length)
 
   return solution(number, permuts, orig_length)
 end
+
+number = [2,3,4]
+
+def iter_solution(number, permuts= [[]], orig_length)
+  number.each do |num|
+    permuts.dup.each do |permut|
+
+    num_letters = NUM_TO_LETTERS[num]
+    num_letters.each do |letter|
+        new_permut = permut.dup
+        new_permut << letter
+        permuts << new_permut
+      end
+    end
+  end
+  permuts.select { |permut| permut.length == orig_length }.uniq
+end
+
+ p iter_solution(number, number.length)
