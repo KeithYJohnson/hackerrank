a9d48bb5f4808109628195a19501e3fffc91de5b
Keith Johnson <keithyjohnson@gmail.com>
2017-05-08 10:11:08 -0400

    just adding in a constraint to the comments that explains why the most obvious solution cant be used

--- index_mult/run.rb
+++ index_mult/run.rb
@@ -1,5 +1,6 @@
 # https://www.interviewcake.com/question/python/product-of-other-numbers
 # Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns a list of the products.
+# NO DIVISON
 #
 # For example, given:
 #
